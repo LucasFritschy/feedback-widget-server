56c96b4b034e95e8bf093bf00920027ad8585868
LucasFritschy <lucasfritschy@gmail.com>
2022-05-05 18:32:11 -0300

    added coverage tests

--- src/use-cases/submit-feedback-use-case.ts
+++ src/use-cases/submit-feedback-use-case.ts
@@ -13,7 +13,21 @@ export class SubmitFeedbackUseCase {
     private mailAdapter: MailAdapter
   ) { }
 
-  async execute({type, comment, screenshot}: SubmitFeedbackUseCaseRequest): Promise<void> {
+  async execute(request: SubmitFeedbackUseCaseRequest): Promise<void> {
+    const { type, comment, screenshot} = request
+
+    if(!type) {
+      throw new Error('Type is required')
+    }
+
+    if(!comment) {
+      throw new Error('Comment is required')
+    }
+
+    if(screenshot && !screenshot.startsWith('data:image/png;base64,')) {
+      throw new Error('Screenshot must be a base64 encoded PNG image')
+    }
+
     await this.feedbacksRepository.create({ type, comment, screenshot })
     await this.mailAdapter.sendMail({
       subject: `Feedback do usuário: ${type}`,
@@ -21,6 +35,7 @@ export class SubmitFeedbackUseCase {
         `<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
         `<p>Novo feedback de ${type}</p>`,
         `<p>Comentário: ${comment}</p>`,
+        `<p>Screenshot: ${screenshot}</p>`,
       ].join('\n')
     })
   }
